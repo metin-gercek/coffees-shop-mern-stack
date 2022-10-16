f7e8b7602902a8448af9f298b9ce41e6fe002713
metin-gercek <metin.gercek@edu.bc.fi>
2022-10-16 22:00:53 +0300

    remove unnecesssary codes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -10,7 +10,6 @@ import Header from "./Header";
 import Shipping from "pages/Shipping";
 import Signup from "pages/Signup";
 import ProfilePage from "pages/ProfilePage";
-//import { LinkContainer } from "react-router-bootstrap/LinkContainer";
 import axios from "axios";
 import { getError } from "../utils";
 import SearchScreen from "pages/SearchPage";
@@ -28,8 +27,6 @@ import OrderPageUser from "pages/OrderPageUser";
 import OrderHistory from "pages/OrderHistory";
 import SearchBox from "./SearchBox";
 import AdminOrderList from "pages/AdminOrderList";
-import { Link } from "react-router-dom";
-
 
 const App = () => {
   const [sidebarIsOpen, setSidebarIsOpen] = useState(false);
@@ -74,12 +71,13 @@ const App = () => {
             </Nav.Item>
             {categories.map((category) => (
               <Nav.Item key={category}>
-                {/*<Link
-                  to={"/search?category="+category}
+                <Nav.Link
+                  href={"/search?category=" + category}
                   onClick={() => setSidebarIsOpen(false)}
-                >*/}
-                  <Nav.Link href={"/search?category="+category} onClick={() => setSidebarIsOpen(false)} className="navbar-text-color">{category}</Nav.Link>
-                {/*</Link>*/}
+                  className="navbar-text-color"
+                >
+                  {category}
+                </Nav.Link>
               </Nav.Item>
             ))}
             <SearchBox />
@@ -123,7 +121,7 @@ const App = () => {
                   </AdminRoute>
                 }
               ></Route>
-			   <Route
+              <Route
                 path="/admin/orders"
                 element={
                   <AdminRoute>
