39af9d6e374c21e290cf457a9a30e71c07132d97
metin-gercek <metin.gercek@edu.bc.fi>
2022-10-17 16:28:48 +0300

    fix product css

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import { Card, Button } from "react-bootstrap";
 import Rating from "./Rating";
-import { useDispatch} from "react-redux";
+import { useDispatch } from "react-redux";
 import { addToCart } from "features/cartSlice";
 
 const Product = (props) => {
@@ -11,32 +11,41 @@ const Product = (props) => {
     dispatch(addToCart(product));
   };
   return (
-    <Card className="product">
-      <Link to={`/product/${product._id}`}>
-        <Card.Img 
-          src={product.image}
-          className="card-img-top pt-3"
-          alt={product.name}
-        />
-      </Link>
-      <Card.Body className="product-info text-center">
-        <Link className="card-title" to={`/product/${product._id}`}>
-          <Card.Title>{product.name}</Card.Title>
-        </Link>
-        <Rating
-          rating={product.rating}
-          numReviews={product.numReviews}
-        ></Rating>
-        <strong>
-          <Card.Text>€ {product.price}</Card.Text>
-        </strong>
-        {product.countInStock === 0 ? (
-          <Button variant="light" disabled onClick={() => handleAddtoCart(product)}>Out of stock</Button>
-        ) : (
-          <Button  onClick={() => handleAddtoCart(product)}>Add to cart</Button>
-        )}
-      </Card.Body>
-    </Card>
+	<Card className="product ">
+	<Link to={`/product/${product._id}`}>
+	  <Card.Img
+		src={product.image}
+		className="card-img-top pt-3"
+		alt={product.name}
+	  />
+	</Link>
+
+	<Card.Body className="product-info text-center">
+	  <Link className="card-title" to={`/product/${product._id}`}>
+		<Card.Title>{product.name}</Card.Title>
+	  </Link>
+	  <Rating
+		rating={product.rating}
+		numReviews={product.numReviews}
+	  ></Rating>
+	  <strong>
+		<Card.Text>€ {product.price}</Card.Text>
+	  </strong>
+	</Card.Body>
+	<div className="productButton">
+	  {product.countInStock === 0 ? (
+		<Button
+		  variant="light"
+		  disabled
+		  onClick={() => handleAddtoCart(product)}
+		>
+		  Out of stock
+		</Button>
+	  ) : (
+		<Button onClick={() => handleAddtoCart(product)}>Add to cart</Button>
+	  )}
+	</div>
+  </Card>
   );
 };
 
